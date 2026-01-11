c997083914f03ef05dccc1e1d0055dde7acbc500
sebas <sebastian11leon@gmail.com>
2026-01-11 14:38:50 -0500

    Add database connection properties and enable table creation in persistence.xml

--- src/main/java/util/JPAUtil.java
+++ src/main/java/util/JPAUtil.java
@@ -55,6 +55,9 @@ public class JPAUtil {
             String url = System.getenv("DB_URL");
             String user = System.getenv("DB_USER");
             String password = System.getenv("DB_PASSWORD");
+            
+
+            
 
             if (url != null) {
                 props.put("jakarta.persistence.jdbc.url", url);
